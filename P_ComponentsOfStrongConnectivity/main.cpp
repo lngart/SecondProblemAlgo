da138531e7ff051ab59481a1a2fd18cc0afa1c6e
Artem <lngaart55@gmail.com>
2021-12-11 23:52:51 +0300

    Corrections

--- P_ComponentsOfStrongConnectivity/main.cpp
+++ P_ComponentsOfStrongConnectivity/main.cpp
@@ -1,142 +1,160 @@
+#include <algorithm>
 #include <iostream>
 #include <memory>
 #include <vector>
 
-template<typename T>
-class IGraph
-{
+template <typename T>
+class IGraph {
 public:
     using vertex = T;
 
     virtual const std::vector<vertex> &get_neighbors(vertex v) const = 0;
     virtual void add_edge(vertex from, vertex to) = 0;
+    virtual std::shared_ptr<IGraph<T>> invert_edges() = 0;
     virtual size_t size() const = 0;
-    virtual ~IGraph() {}
+    virtual ~IGraph() {
+    }
 };
 
-template<typename T>
-class ListGraph : public IGraph<T>
-{
+template <typename T>
+class ListGraph : public IGraph<T> {
     using typename IGraph<T>::vertex;
 
     std::vector<std::vector<vertex>> vertices;
 
 public:
-    ListGraph(size_t num_vertices) : vertices(num_vertices) {}
+    ListGraph(size_t num_vertices) : vertices(num_vertices) {
+    }
 
-    const std::vector<vertex> &get_neighbors(vertex v) const override { return vertices[v]; }
+    const std::vector<vertex> &get_neighbors(vertex v) const override {
+        return vertices[v];
+    }
 
-    void add_edge(vertex from, vertex to) override { vertices[from].push_back(to); }
+    void add_edge(vertex from, vertex to) override {
+        vertices[from].push_back(to);
+    }
 
-    inline size_t size() const override { return vertices.size(); }
+    std::shared_ptr<IGraph<T>> invert_edges() override {
+        std::shared_ptr<IGraph<T>> graph_with_inverted_edges = std::make_shared<ListGraph<T>>(vertices.size());
+
+        for (vertex v = 0; v < vertices.size(); ++v)
+            for (const auto &neighbor : vertices[v])
+                graph_with_inverted_edges->add_edge(neighbor, v);
+
+        return graph_with_inverted_edges;
+    }
+
+    inline size_t size() const override {
+        return vertices.size();
+    }
 };
 
 enum class Color { white, black };
 
-template<typename T>
-void topological_sort(std::unique_ptr<IGraph<T>> &graph,
-                      T vertex,
-                      std::vector<Color> &colors,
-                      std::vector<T> &sorted_vertices)
-{
+template <typename T>
+void dfs(IGraph<T> &graph, T vertex, std::vector<Color> &colors, std::vector<T> &sorted_vertices) {
     colors[vertex] = Color::black;
-    for (const auto &neighbor : graph->get_neighbors(vertex))
+
+    for (const auto &neighbor : graph.get_neighbors(vertex))
         if (colors[neighbor] == Color::white)
-            topological_sort(graph, neighbor, colors, sorted_vertices);
+            dfs(graph, neighbor, colors, sorted_vertices);
 
     sorted_vertices.push_back(vertex);
 }
 
-template<typename T>
-void create_component_strong_connectivity(std::unique_ptr<IGraph<T>> &graph,
-                                          T vertex,
-                                          std::vector<Color> &colors,
-                                          std::vector<T> &component_strong_connectivity)
-{
+template <typename T>
+void arrange_vertices(IGraph<T> &graph, std::vector<T> &sorted_vertices) {
+    std::vector<Color> colors(graph.size(), Color::white);
+
+    for (T vertex{0}; vertex < graph.size(); ++vertex)
+        if (colors[vertex] == Color::white)
+            dfs(graph, vertex, colors, sorted_vertices);
+
+    std::reverse(sorted_vertices.begin(), sorted_vertices.end());
+}
+
+template <typename T>
+void create_component_strong_connectivity(IGraph<T> &graph, T vertex, std::vector<Color> &colors,
+                                          std::vector<T> &component_strong_connectivity) {
     colors[vertex] = Color::black;
 
     component_strong_connectivity.push_back(vertex);
 
-    for (const auto &neighbor : graph->get_neighbors(vertex))
+    for (const auto &neighbor : graph.get_neighbors(vertex))
         if (colors[neighbor] == Color::white)
-            create_component_strong_connectivity(graph,
-                                                 neighbor,
-                                                 colors,
-                                                 component_strong_connectivity);
+            create_component_strong_connectivity(graph, neighbor, colors, component_strong_connectivity);
 }
 
-template<typename T>
-void get_component_strong_connectivity(std::unique_ptr<IGraph<T>> &graph,
-                                       std::unique_ptr<IGraph<T>> &graph_with_inverted_edges)
-{
-    std::vector<Color> colors(graph->size(), Color::white);
+template <typename T>
+void find_component_strong_connectivity(IGraph<T> &graph, std::vector<T> &number_of_component_strong_connectivity) {
+    auto graph_with_inverted_edges = graph.invert_edges();
+
     std::vector<T> sorted_vertices;
+    std::vector<Color> colors(graph.size(), Color::white);
 
-    for (T vertex{0}; vertex < graph->size(); ++vertex)
-        if (colors[vertex] == Color::white)
-            topological_sort(graph, vertex, colors, sorted_vertices);
+    arrange_vertices(graph, sorted_vertices);
 
-    colors.assign(colors.size(), Color::white);
-
-    std::vector<T> number_of_component_strong_connectivity(graph->size());
     T cnt_component_strong_connectivity{1};
 
     for (T vertex{0}; vertex < graph_with_inverted_edges->size(); ++vertex) {
-        auto sorted_vertex = sorted_vertices[(sorted_vertices.size() - 1) - vertex];
+        auto sorted_vertex = sorted_vertices[vertex];
         if (colors[sorted_vertex] == Color::white) {
             std::vector<T> component_strong_connectivity;
-            create_component_strong_connectivity(graph_with_inverted_edges,
-                                                 sorted_vertex,
-                                                 colors,
+
+            create_component_strong_connectivity(*graph_with_inverted_edges, sorted_vertex, colors,
                                                  component_strong_connectivity);
 
-            for (auto it = component_strong_connectivity.begin();
-                 it != component_strong_connectivity.end();
-                 ++it)
-                number_of_component_strong_connectivity[*it] = cnt_component_strong_connectivity;
+            for (const auto &v : component_strong_connectivity)
+                number_of_component_strong_connectivity[v] = cnt_component_strong_connectivity;
+
             ++cnt_component_strong_connectivity;
         }
     }
+}
 
-    std::cout << cnt_component_strong_connectivity - 1 << std::endl;
+template <typename T>
+T find_quantity_of_components_strong_connectivity(std::vector<T> &number_of_component_strong_connectivity) {
+    return *std::max_element(number_of_component_strong_connectivity.begin(),
+                             number_of_component_strong_connectivity.end());
+}
+
+template <typename T>
+void print_component_strong_connectivity(IGraph<T> &graph) {
+    std::vector<T> number_of_component_strong_connectivity(graph.size());
+
+    find_component_strong_connectivity(graph, number_of_component_strong_connectivity);
+
+    const auto quantity_of_components_strong_connectivity =
+        find_quantity_of_components_strong_connectivity(number_of_component_strong_connectivity);
+    std::cout << quantity_of_components_strong_connectivity << std::endl;
+
     for (size_t i{0}; i < number_of_component_strong_connectivity.size(); ++i)
         std::cout << number_of_component_strong_connectivity[i] << " ";
 }
 
-template<typename T>
-void initialization(std::unique_ptr<IGraph<T>> &graph,
-                    std::unique_ptr<IGraph<T>> &graph_with_inverted_edges,
-                    size_t &num_vertices,
-                    size_t &num_edges)
-{
+void initialization(size_t &num_vertices, size_t &num_edges) {
     std::cin >> num_vertices >> num_edges;
-    graph = std::make_unique<ListGraph<T>>(num_vertices);
-    graph_with_inverted_edges = std::make_unique<ListGraph<T>>(num_vertices);
 }
 
-template<typename T>
-void adding_edge(std::unique_ptr<IGraph<T>> &graph,
-                 std::unique_ptr<IGraph<T>> &graph_with_inverted_edges,
-                 size_t num_edges)
-{
+template <typename T>
+void adding_edge(IGraph<T> &graph, size_t num_edges) {
     for (size_t i{0}; i < num_edges; ++i) {
         T from{0}, to{0};
         std::cin >> from >> to;
-        graph->add_edge(from - 1, to - 1);                     // We use numbering from 0
-        graph_with_inverted_edges->add_edge(to - 1, from - 1); // We use numbering from 0
+        graph.add_edge(from - 1, to - 1);  // We use numbering from 0
     }
 }
 
-int main()
-{
+int main() {
     size_t num_vertices{0}, num_edges{0};
-    std::unique_ptr<IGraph<size_t>> graph, graph_with_inverted_edges;
 
-    initialization(graph, graph_with_inverted_edges, num_vertices, num_edges);
+    initialization(num_vertices, num_edges);
 
-    adding_edge(graph, graph_with_inverted_edges, num_edges);
+    ListGraph<size_t> graph(num_vertices);
 
-    get_component_strong_connectivity(graph, graph_with_inverted_edges);
+    adding_edge(graph, num_edges);
+
+    print_component_strong_connectivity(graph);
 
     return 0;
 }
