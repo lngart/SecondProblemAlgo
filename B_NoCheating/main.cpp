d431567225a60b4d109096937a6d5a4f933ec6d0
Artem <lngaart55@gmail.com>
2021-12-11 23:18:25 +0300

    Corrections

--- B_NoCheating/main.cpp
+++ B_NoCheating/main.cpp
@@ -5,109 +5,114 @@
 #include <vector>
 #include <queue>
 
-template<typename T>
-class IGraph
-{
+template <typename T>
+class IGraph {
 public:
     using vertex = T;
 
     virtual std::vector<vertex> get_neighbors(vertex v) const = 0;
     virtual void add_edge(vertex from, vertex to) = 0;
     virtual size_t size() const = 0;
-    virtual ~IGraph() {}
+    virtual ~IGraph() {
+    }
 };
 
-template<typename T>
-class ListGraph : public IGraph<T>
-{
+template <typename T>
+class ListGraph : public IGraph<T> {
     using typename IGraph<T>::vertex;
     std::vector<std::vector<vertex>> vertices;
 
 public:
-    ListGraph(size_t num_vertices) : vertices(num_vertices) {}
+    ListGraph(size_t num_vertices) : vertices(num_vertices) {
+    }
 
-    std::vector<vertex> get_neighbors(vertex v) const override { return vertices.at(v); }
+    std::vector<vertex> get_neighbors(vertex v) const override {
+        return vertices.at(v);
+    }
 
-    void add_edge(vertex from, vertex to) override
-    {
+    void add_edge(vertex from, vertex to) override {
         vertices.at(from).push_back(to);
         vertices.at(to).push_back(from);
     }
 
-    inline size_t size() const override { return vertices.size(); }
+    inline size_t size() const override {
+        return vertices.size();
+    }
 };
 
 enum class Color { white, red, blue };
 
-template<typename T>
-bool is_bipartite(std::unique_ptr<IGraph<T>> &graph, std::vector<Color> &colors, T starting_vertex)
-{
+Color get_invert_color(Color color) {
+    if (color == Color::blue)
+        return Color::red;
+    else if (color == Color::red)
+        return Color::blue;
+    else
+        return color;
+}
+
+template <typename T>
+bool BFS(IGraph<T> &graph, std::vector<Color> &colors, T starting_vertex) {
     colors[starting_vertex] = Color::red;
 
     std::queue<T> vertices_queue;
     vertices_queue.push(starting_vertex);
+
     while (!vertices_queue.empty()) {
         auto top_vertex = vertices_queue.front();
         vertices_queue.pop();
-        auto neighbors = graph->get_neighbors(top_vertex);
+
+        auto neighbors = graph.get_neighbors(top_vertex);
         for (const auto &v : neighbors) {
-            if (colors[top_vertex] == Color::red) {
-                if (colors[v] == Color::white) {
-                    vertices_queue.push(v);
-                    colors[v] = Color::blue;
-                } else if (colors[v] == Color::red)
-                    return false;
-            } else if (colors[top_vertex] == Color::blue) {
-                if (colors[v] == Color::white) {
-                    vertices_queue.push(v);
-                    colors[v] = Color::red;
-                } else if (colors[v] == Color::blue)
-                    return false;
-            }
+            if (colors[v] == Color::white) {
+                vertices_queue.push(v);
+                colors[v] = get_invert_color(colors[top_vertex]);
+            } else if (colors[v] == colors[top_vertex])
+                return false;
         }
     }
     return true;
 }
 
-template<typename T>
-void initialization(std::unique_ptr<IGraph<T>> &graph, size_t &num_vertices, size_t &num_edges)
-{
-    std::cin >> num_vertices >> num_edges;
-    graph = std::make_unique<ListGraph<T>>(num_vertices);
+template <typename T>
+bool is_bipartite_graph(IGraph<T> &graph) {
+    bool is_bipartite{true};
+    std::vector<Color> colors(graph.size(), Color::white);
+
+    for (T current_vertex{0}; current_vertex < graph.size() && is_bipartite; ++current_vertex)
+        if (colors[current_vertex] == Color::white)
+            is_bipartite = is_bipartite && BFS(graph, colors, current_vertex);
+
+    return is_bipartite;
 }
 
-template<typename T>
-void adding_edge(std::unique_ptr<IGraph<T>> &graph, size_t num_edges)
-{
-    for (size_t i{0}; i < num_edges; ++i) {
+template <typename T>
+void print_is_possible_separate_students(IGraph<T> &students) {
+    std::cout << (is_bipartite_graph(students) ? "YES" : "NO");
+}
+
+void initialization(size_t &num_students, size_t &num_pairs_students) {
+    std::cin >> num_students >> num_pairs_students;
+}
+
+template <typename T>
+void build_graph_from_pairs_students(IGraph<T> &students, size_t num_pairs_students) {
+    for (size_t i{0}; i < num_pairs_students; ++i) {
         T from{0}, to{0};
         std::cin >> from >> to;
-        graph->add_edge(from - 1, to - 1); // We use numbering from 0
+        students.add_edge(from - 1, to - 1);  // We use numbering from 0
     }
 }
 
-template<typename T>
-inline void is_possible_separate_students(std::unique_ptr<IGraph<T>> &graph)
-{
-    bool is_possible{true};
-    std::vector<Color> colors(graph->size(), Color::white);
-    for (T current_vertex{0}; current_vertex < graph->size() && is_possible; ++current_vertex)
-        if (colors[current_vertex] == Color::white)
-            is_possible = is_possible && is_bipartite(graph, colors, current_vertex);
-
-    std::cout << (is_possible ? "YES" : "NO");
-}
-
-int main()
-{
+int main() {
     size_t num_students{0}, num_pairs_students{0};
-    std::unique_ptr<IGraph<size_t>> students;
+    initialization(num_students, num_pairs_students);
 
-    initialization(students, num_students, num_pairs_students);
+    ListGraph<size_t> students(num_students);
 
-    adding_edge(students, num_pairs_students);
+    build_graph_from_pairs_students(students, num_pairs_students);
 
-    is_possible_separate_students(students);
+    print_is_possible_separate_students(students);
 
     return 0;
 }
